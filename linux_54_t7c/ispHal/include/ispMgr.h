e2b6d3f6ba7114709473b114910b6d11416c0122
jin.wang <jin.wang@amlogic.com>
2025-11-10 14:01:09 +0800

    t7c: add isp param control [1/1]
    PD#SH-25553
    
    Problem:
    add isp param property control
    
    Solution:
    add isp param property control
    
    Verify:
    local
    
    Signed-off-by: jin.wang <jin.wang@amlogic.com>
    Change-Id: I1f1fcd0c0ba30bc4e0fb4b2f9d81553ff8801a69

--- linux_54_t7c/ispHal/include/ispMgr.h
+++ linux_54_t7c/ispHal/include/ispMgr.h
@@ -81,6 +81,8 @@ class IspMgr {
     //int getAWBInfo(void* data);
     //int getAEInfo(void* data);
     int setMaxfps(int fps);
+    int set_awb(int awb);
+    int set_csc(int brightness, int contrast);
   public:
     static struct ispIF  mIspIF;
   protected:
