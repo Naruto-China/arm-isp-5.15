117c002477e266210efeefad0eb8a9d87881bf8b
shuanshuan.jiang <shuanshuan.jiang@amlogic.com>
2025-11-16 12:05:14 +0800

    camera: add 2 lane setting of imx290 [1/1]
    PD#SWPL-231068
    
    Problem:
    add new 2 lane setting of imx290
    
    Solution:
    add 2 lane setting of imx290
    
    Verify:
    local
    
    Signed-off-by: shuanshuan.jiang <shuanshuan.jiang@amlogic.com>
    Change-Id: Ie1bec662ce8b190f0b11c460d036834c5aacdca2

--- linux_54_t7c/driver/amlsens/imx290/aml_imx290.c
+++ linux_54_t7c/driver/amlsens/imx290/aml_imx290.c
@@ -51,19 +51,9 @@ static const struct imx290_mode imx290_modes_2lanes[] = {
 		.width = 1920,
 		.height = 1080,
 		.hmax = 0x1130,
-		.data = imx290_1080p_settings,
-		.data_size = ARRAY_SIZE(imx290_1080p_settings),
-
+		.data = linear_1080_30fps_446Mbps_2lane_12bits,
+		.data_size = ARRAY_SIZE(linear_1080_30fps_446Mbps_2lane_12bits),
 		.link_freq_index = FREQ_INDEX_1080P,
-	},
-	{
-		.width = 1280,
-		.height = 720,
-		.hmax = 0x19c8,
-		.data = imx290_720p_settings,
-		.data_size = ARRAY_SIZE(imx290_720p_settings),
-
-		.link_freq_index = FREQ_INDEX_720P,
 	},
 };
 
@@ -833,6 +823,7 @@ int imx290_init(struct i2c_client *client, void *sdrv)
 	imx290->client->addr = IMX290_SLAVE_ID;
 	imx290->gpio = &sensor->gpio;
 	imx290->fps = 30;
+	imx290->nlanes = 4;
 
 	imx290->regmap = devm_regmap_init_i2c(client, &imx290_regmap_config);
 	if (IS_ERR(imx290->regmap)) {
